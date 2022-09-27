5b932bb689efd632301b03425df836b1cca1a71f
jhs512 <jangka512@gmail.com>
2022-09-27 23:41:52 +0900

    N + 1 문제 해결

--- src/main/java/com/ll/exam/app10/app/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/app10/app/article/controller/ArticleController.java
@@ -36,10 +36,7 @@ public class ArticleController {
     @GetMapping("/list")
     public String showList(Model model) {
         List<Article> articles = articleService.getArticles();
-
-        for ( Article article : articles ) {
-            articleService.loadForPrintData(article);
-        }
+        articleService.loadForPrintData(articles);
 
         model.addAttribute("articles", articles);
 
